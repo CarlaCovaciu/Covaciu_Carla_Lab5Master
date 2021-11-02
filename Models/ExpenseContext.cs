60183ab09e0a5d4d7d5c617e274d570fc718b5ea
Covaciu Carla <covaciu_carla99@yahoo.com>
2021-11-02 19:52:53 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Covaciu_Carla_Lab5.Models;
+
+namespace Covaciu_Carla_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options) : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+    }
+}
